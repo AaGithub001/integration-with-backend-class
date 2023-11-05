0caaca6138053528d2554fcf29cecf6e757e6d66
AaGithub001 <aasigning001@gmail.com>
2023-11-05 14:59:57 +0545

    code-update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[categories, setCategories] = useState([]);
+  const[products, setProducts] = useState([]);
+  const[singleproduct,setSingleproduct] = useState([]);
+  fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> setProducts(json))
+
+  fetch('https://fakestoreapi.com/products/categories')
+            .then(res => res.json())
+            .then(json => setCategories(json))
+
+  fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>setSingleproduct(json))       
+  
+  fetch('https://fakestoreapi.com/products?sort=desc')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <ul>
+        <h1>Categories</h1>
+      {categories.map(category => (
+        <li>{category}</li>
+        ))
+      }
+      </ul>
+      <ul>
+        <h1>All Products</h1>
+        {products.map(product =>(
+          <div key={product.id} style={{margin:"50px"}}>
+          <li > Title : {product.title} </li>
+          <li>Description :{product.description}</li>
+          <li>Price: $ {product.price}</li>
+          </div>
+        ))
+        } 
+      </ul>  
+      <ul>
+          <div style={{margin:"50px"}}> 
+          <h1> First Product</h1>
+          <li > Category : {singleproduct.category} </li>
+          <li>Description :{singleproduct.description}</li>
+          <li>Image :{singleproduct.image}</li>
+          <li>Price: $ {singleproduct.price}</li>
+          <li>Count :{singleproduct.rating?.count}</li>
+          <li>Rate :{singleproduct.rating?.rate}</li>
+          <li>Title :{singleproduct.title}</li>
+          </div>
+    
+      </ul>  
+
     </div>
   );
 }
